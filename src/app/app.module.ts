a715bdef063b23ce3dd975aaf8a638f8da2a0323
Simon Rojas <sdavid.rojas@hotmail.com>
2024-05-23 16:15:31 -0500

    Creación del servicio de Heroes para obtener la data desde el backend, desarrollo y maquetación de los compoenentes list-page y card, para mostrar los heroes, así como creación del pipe personalizado heroe-image para parametrizar la ruta de las imagenes y sea mantenible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,9 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+
+    HttpClientModule
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+
+  public heroes: Hero[] = [];
+
+  constructor( private heroesService: HeroesService ){}
+
+  ngOnInit(): void {
+    this.heroesService.getHero()
+    .subscribe( heroes => { this.heroes = heroes });
+  }
 
 }

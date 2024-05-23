1943aa7fa9ca26c286877b77cde4f0ed8e0f3c26
Simon Rojas <sdavid.rojas@hotmail.com>
2024-05-23 10:25:20 -0500

    Definición de rutas hijas y lazyLoad de los modúlos auth y heroes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,8 +2,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutPagesComponent,
+        children: [
+            { path: 'login',        component: LoginPageComponent },
+            { path: 'new-account',  component: RegisterPageComponent },
+            { path: '**',           redirectTo: 'login' },
+        ]
+    },
+    
 ]
 
 
